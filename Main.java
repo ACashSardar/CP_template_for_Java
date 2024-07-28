28aa4f80f4c1704f0ec0427915bfa4cb691e8116
Akash <akashsardar383@gmail.com>
2024-07-28 08:59:47 +0530

    ok

--- Main.java
+++ Main.java
@@ -1,12 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 /***
  * ----------------------------------------------------------------
--- templates/SegmentTree.java
+++ templates/SegmentTree.java
@@ -18,6 +18,52 @@ public class SegmentTree {
             int mid = lo + (hi - lo) / 2;
             build(2 * ind + 1, lo, mid, arr);
             build(2 * ind + 2, mid + 1, hi, arr);
+            seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+        }
+
+        public int query(int ind, int lo, int hi, int l, int r, int[] arr) {
+            if (lo >= l && hi <= r) {
+                return seg[ind];
+            } else if (hi < l || lo > r) {
+                return 0;
+            }
+            int mid = lo + (hi - lo) / 2;
+            int left = query(2 * ind + 1, lo, mid, l, r, arr);
+            int right = query(2 * ind + 2, mid + 1, hi, l, r, arr);
+            return left + right;
+        }
+
+        public void update(int ind, int lo, int hi, int i, int val, int[] arr) {
+            if (lo == hi) {
+                seg[ind] = arr[i] = val;
+                return;
+            }
+            int mid = lo + (hi - lo) / 2;
+            if (i <= mid) {
+                update(2 * ind + 1, lo, mid, i, val, arr);
+            } else {
+                update(2 * ind + 2, mid + 1, hi, i, val, arr);
+            }
+            seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
+        }
+    }
+
+    static class SegTreeMax {
+
+        int[] seg;
+
+        public SegTreeMax(int size) {
+            seg = new int[size + 1];
+        }
+
+        public void build(int ind, int lo, int hi, int[] arr) {
+            if (lo == hi) {
+                seg[ind] = arr[lo];
+                return;
+            }
+            int mid = lo + (hi - lo) / 2;
+            build(2 * ind + 1, lo, mid, arr);
+            build(2 * ind + 2, mid + 1, hi, arr);
             seg[ind] = Math.max(seg[2 * ind + 1], seg[2 * ind + 2]);
         }
 
@@ -122,7 +168,7 @@ public class SegmentTree {
         // ----index: 0, 1, 2, 3, 4, 5, 6, 7, 8
         int[] arr = { 6, 2, 1, 4, 7, 3, 8, 9, 5 };
         int n = arr.length;
-        SegTree st = new SegTree(4 * n);
+        SegTreeMax st = new SegTreeMax(4 * n);
         st.build(0, 0, n - 1, arr);
         int l = 0, r = 5;
 
